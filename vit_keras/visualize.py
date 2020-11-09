9d9ea65b04aec83e50efdf6cc97ed605d30f14e7
Fausto Morales <faustomorales@gmail.com>
2020-11-09 10:13:12 -0600

    Add identity matrix to attention maps for visualization.

--- vit_keras/visualize.py
+++ vit_keras/visualize.py
@@ -35,11 +35,18 @@ def attention_map(model, image):
     # Average the attention weights across all heads.
     reshaped = reshaped.mean(axis=1)
 
+    # From Section 3 in https://arxiv.org/pdf/2005.00928.pdf ...
+    # To account for residual connections, we add an identity matrix to the
+    # attention matrix and re-normalize the weights.
+    reshaped = reshaped + np.eye(reshaped.shape[1])
+    reshaped = reshaped / reshaped.sum(axis=(1, 2))[:, np.newaxis, np.newaxis]
+
     # Recursively multiply the weight matrices
     v = reshaped[-1]
     for n in range(1, len(reshaped)):
         v = np.matmul(v, reshaped[-1 - n])
 
+    # Attention from the output token to the input space.
     mask = v[0, 1:].reshape(grid_size, grid_size)
     mask = cv2.resize(mask / mask.max(), image.shape[:2])[..., np.newaxis]
     return (mask * image).astype("uint8")
