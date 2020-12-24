30ad4d15c78026550391484f61913089874f604a
Fausto Morales <faustomorales@gmail.com>
2020-12-23 19:06:25 -0600

    Fix formatting.

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -4,10 +4,11 @@ import requests
 
 from vit_keras import vit
 
+
 def test_urls():
     """Ensure that all the weights URLs work."""
-    for weights, size in itertools.product(vit.WEIGHTS, vit.SIZES): # type: ignore
+    for weights, size in itertools.product(vit.WEIGHTS, vit.SIZES):  # type: ignore
         with requests.get(
-            f"{vit.BASE_URL}/ViT-{size}_{weights}.npz", stream=True # type: ignore
+            f"{vit.BASE_URL}/ViT-{size}_{weights}.npz", stream=True  # type: ignore
         ) as r:
             assert r.status_code == 200
